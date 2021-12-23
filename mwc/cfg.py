882beb2f715623853ddaef787c6c8727b8d669ae
Bruno Geninatti <brunogeninatti@gmail.com>
2021-12-22 22:20:42 -0300

    refactor: add DATABASE_URL con configuration variables

--- mwc/cfg.py
+++ mwc/cfg.py
@@ -1,14 +1,22 @@
 
 import os
+from urllib.parse import urlparse
 
 
 def load_config():
+    params = urlparse(os.environ["DATABASE_URL"])
     config = {
         'SRT_FOLDER': clean_setting('SRT_FOLDER'),
         'PNG_FOLDER': clean_setting('PNG_FOLDER'),
         'DEFAULT_LANGUAGE_ID': clean_setting('DEFAULT_LANGUAGE_ID'),
-        'DB_PATH': clean_setting('DB_PATH'),
         'STOP_WORDS_JSON_FILE': clean_setting('STOP_WORDS_JSON_FILE'),
+        'DB': {
+            'db_name': params.path[1:],
+            'user': params.username,
+            'password': params.password,
+            'host': params.hostname,
+            'port': params.port,
+        },
         'TWITTER_CREDENTIALS': {
             'consumer_key': clean_setting('TWITTER_CONSUMER_KEY'),
             'consumer_secret': clean_setting('TWITTER_CONSUMER_SECRET'),
