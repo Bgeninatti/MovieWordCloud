02ffe056259b8fc8bb7d828a22d82617f9967f5f
Bruno Geninatti <brunogeninatti@gmail.com>
2021-12-27 09:32:11 -0300

    refactor(twitter): implement WordCloudService in CLI

--- mwc/bots/cli.py
+++ mwc/bots/cli.py
@@ -3,10 +3,9 @@ import logging
 
 from mwc.core.cfg import load_config
 
-from mwc.core.helpers import get_stop_words
 from mwc.core.db.queries import get_next_movie
-from mwc.core.wc.wordcloud import WordCloud
 from .twitter import TwitterClient
+from mwc.core.wc.service import WordCloudService
 
 log = logging.getLogger(__name__)
 CONFIG = load_config()
@@ -21,10 +20,9 @@ def tweet_movie(srt_folder, language, twitter_credetials, twitter_account_name):
     """
     Tweets a random movie stored in the local database.
     """
+    wc_service = WordCloudService(srt_folder)
+    twitter_client = TwitterClient(**twitter_credetials)
     movie = get_next_movie()
     log.info("Selected movie: Name='%s', LanguageId='%s'", movie.original_title, language)
-    stop_words = get_stop_words()
-    wc = WordCloud(movie, stop_words, srt_folder)
-    wc.to_file()
-    client = TwitterClient(**twitter_credetials)
-    client.tweet_wordcloud(movie, wc.filename, twitter_account_name)
+    wc = wc_service.build_from_movie(movie)
+    twitter_client.tweet_wordcloud(movie, wc.filename, twitter_account_name)
