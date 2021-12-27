ed82276403c5a8285efbc5a817cb3ba51dbda470
Bruno Geninatti <brunogeninatti@gmail.com>
2021-12-27 08:41:20 -0300

    refactor(subtitles): Implement SubtitlesService in CLI

--- mwc/subtitles/cli.py
+++ mwc/subtitles/cli.py
@@ -1,11 +1,9 @@
-
 import click
 import logging
 
 from mwc.cfg import load_config
 
-from .opensubtitles import OpenSubtitles
-from ..db.queries import get_movies_without_subtitles
+from .service import SubtitlesService
 
 log = logging.getLogger(__name__)
 CONFIG = load_config()
@@ -18,13 +16,5 @@ def download_missing_subtitles(srt_folder, language):
     """
     Downloads subtitles for movies in the local database that doesn't have one yet.
     """
-    movies = get_movies_without_subtitles()
-    os_client = OpenSubtitles(srt_folder, language)
-    log.info("Movies with missing subtitles: movies_count=%s", len(movies))
-
-    for movie in movies:
-        subtitle = os_client.get_valid_subtitle(movie)
-        if subtitle:
-            movie.opensubtittle_id = subtitle.subtitle_id
-            movie.srt_file = subtitle.srt_location
-            movie.save()
+    service = SubtitlesService(srt_folder=srt_folder, language=language)
+    service.sync()
